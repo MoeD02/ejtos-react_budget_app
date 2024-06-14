6cbc800fe4105a696d03c0a153ad51b78201df6b
MoeD02 <dahbourmohammad3@gmail.com>
2024-06-13 23:16:05 -0700

    finished project

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -2,15 +2,36 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const { dispatch,budget, currency } = useContext(AppContext);
     const [newBudget, setNewBudget] = useState(budget);
+    
     const handleBudgetChange = (event) => {
         setNewBudget(event.target.value);
+        dispatch({
+            type: 'SET_BUDGET',
+            payload: newBudget,
+        })
     }
+    const handleCurrencyChange = (event) => {
+       //CHG_CURRENCY
+        dispatch({
+            type: 'CHG_CURRENCY',
+            payload: event.target.value,
+        })
+        
+    };
     return (
 <div className='alert alert-secondary'>
-<span>Budget: £{budget}</span>
+<span>Budget: {currency}</span>
 <input type="number" step="10" value={newBudget} onChange={handleBudgetChange}></input>
+<br/>
+ <select value={currency} onChange={handleCurrencyChange}style={{ backgroundColor: '#d4edda' }}>
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Ruppee</option>
+            </select>
+
 </div>
     );
 };
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,7 @@ import { TiDelete } from 'react-icons/ti';
 import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    const { dispatch } = useContext(AppContext);
+    const { dispatch,currency } = useContext(AppContext);
 
     const handleDeleteExpense = () => {
         dispatch({
@@ -24,12 +24,41 @@ const ExpenseItem = (props) => {
         });
 
     }
+     const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense
+        });
+
+    }
 
     return (
         <tr>
         <td>{props.name}</td>
-        <td>£{props.cost}</td>
-        <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
+        <td>{currency}{props.cost}</td>
+        <td><button onClick={event=> increaseAllocation(props.name)}  style={{
+                        backgroundImage: `url('/plus-297823_1280.png')`, 
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        border: 'none',
+                        width: '30px',
+                        height: '30px',
+                        cursor: 'pointer'
+                    }}></button></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)} style={{
+                        backgroundImage: `url('/minus.jpeg')`, 
+                        backgroundSize: 'cover',
+                        backgroundPosition: 'center',
+                        border: 'none',
+                        width: '30px',
+                        height: '30px',
+                        cursor: 'pointer'
+                    }}></button></td>
         <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
         </tr>
     );
